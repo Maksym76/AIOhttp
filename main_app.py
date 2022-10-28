1231ac26d162946c9d0280f785bc5761ba575f63
Maksym76 <kobzarenkomaksym@gmail.com>
2022-10-28 20:36:19 +0300

    version 2

--- main_app.py
+++ main_app.py
@@ -22,9 +22,10 @@ async def short_url_get(request):
 async def short_url_post(request):
     result = await request.text()
     user_url = result.replace('user_url=', '')
+    user_url_list = user_url.split('://')
     db = request.app["db"]
     collection = db['shortener']
-    url_record = await collection.insert_one({'user_url': user_url})
+    url_record = await collection.insert_one({'user_url': user_url_list[1], 'prefix': user_url_list[0]})
     return web.Response(text=str(url_record.inserted_id))
 
 
@@ -32,9 +33,10 @@ async def handler(request):
     name_url = request.match_info.get("name")
     db = request.app["db"]
     collection = db['shortener']
-    find_url = await collection.find_one({'_id': ObjectId(name_url)})
-    select_url = find_url['user_url']
-    return web.HTTPFound('http://' + select_url)
+    obj_url = await collection.find_one({'_id': ObjectId(name_url)})
+    prefix = obj_url.get("prefix", 'http')
+    url = obj_url.get['user_url']
+    return web.HTTPFound(prefix + '://' + url)
 
 
 db = asyncio.run(setup_db())
